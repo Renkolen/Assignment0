e6505307ba6b38c349bc57df8d85131fca423401
Renkolen <65435267+Renkolen@users.noreply.github.com>
2020-05-16 16:11:22 -0700

    Add files via upload

---
+++ Assignment_0.py
@@ -0,0 +1,9 @@
+x = int(input('Enter a number for x'))
+y = int(input('Please enter a number for y'))
+print("Here you go!")
+print(x ** y)
+import math
+
+x = int(input('Now just enter just one more number'))
+print("Ta da!")
+print(math.log2(x))
