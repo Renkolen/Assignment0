d0e70905fc6c3bc939e329933ca31cfc1b6d6a74
Renkolen <65435267+Renkolen@users.noreply.github.com>
2020-05-16 18:06:44 -0700

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,25 @@
+phrase = "Giraffe Whipper"
+print(phrase + " is getting arrested!")
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("W"))
+print(phrase.replace("Giraffe", "Loli"))
+print(6.09)
+print(543/34)
+print(434 % 32)
+my_num = 18
+print(str(my_num) + " my favorite age!")
+my_num = -5
+print(abs(my_num))
+print(pow(6, 9))
+print(max(4, 6))
+print(min(54,332))
+print(round(4.20))
+
+from math import *
+
+print(floor(3.472831))
+print(ceil(9.3245))
+print(sqrt(144))
