6e8cacb21ab50a214befb1f69e6cff58ddb236c9
Renkolen <65435267+Renkolen@users.noreply.github.com>
2020-05-16 17:52:22 -0700

    Add files via upload

--- Assignment_0.py
+++ Assignment_0.py
@@ -1,9 +1,9 @@
 x = int(input('Enter a number for x'))
 y = int(input('Please enter a number for y'))
-print("Here you go!")
-print(x ** y)
+print("So " + str(x) + " to the power of " + str(y) + " equals " + str(int(x) ** int(y)))
+print("Wow look at all that math! :D")
 import math
 
-x = int(input('Now just enter just one more number'))
+x = int(input("Ok so now we're gonna get crazy. Give me another number." ))
 print("Ta da!")
 print(math.log2(x))
